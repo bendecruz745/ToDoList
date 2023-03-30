18ffef8c6b3ca8079d0eac711619fa9ff5514136
Ben De Cruz <decruz_ben@hotmail.com>
2023-03-30 16:03:49 +1100

    cleaning up some old/prev code

--- src/Components/ListItem.js
+++ src/Components/ListItem.js
@@ -3,31 +3,23 @@ import { useState } from "react";
 import { Button } from "react-bootstrap";
 
 function ListItem({ item, index, setItems }) {
-  const [visible, setVisible] = useState(true);
-  let itemList = [];
-
   function handleDelete(event) {
-    itemList = JSON.parse(localStorage.getItem("Items"));
-    itemList.splice(index, 1);
-    localStorage.setItem("Items", JSON.stringify(itemList));
+    let itemList = JSON.parse(localStorage.getItem("Items"));
+    localStorage.setItem("Items", JSON.stringify(itemList.splice(index, 1)));
     setItems(itemList);
   }
 
   return (
-    <>
-      {visible && (
-        <div className="d-flex">
-          <li className="d-flex">{item}</li>
-          <Button
-            as="input"
-            type="submit"
-            value="Delete"
-            onClick={handleDelete}
-            className="ms-3"
-          />
-        </div>
-      )}
-    </>
+    <div className="d-flex">
+      <li className="d-flex">{item}</li>
+      <Button
+        as="input"
+        type="submit"
+        value="Delete"
+        onClick={handleDelete}
+        className="ms-3"
+      />
+    </div>
   );
 }
 
